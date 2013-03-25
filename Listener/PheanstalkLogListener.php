d8fbe56fc39b5252514612f531c81af3fd19a748
Thomas Tourlourat <thomas@tourlourat.com>
2013-03-25 16:29:13 +0100

    Use listener to log pheanstalk activity

--- Listener/PheanstalkLogListener.php
+++ Listener/PheanstalkLogListener.php
@@ -2,74 +2,94 @@
 
 namespace Leezy\PheanstalkBundle\Listener;
 
-use Leezy\PheanstalkBundle\Event\CommandEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
-class PheanstalkListener implements EventSubscriber
+use Leezy\PheanstalkBundle\Event\CommandEvent;
+use Leezy\PheanstalkBundle\Proxy\PheanstalkProxyInterface;
+
+class PheanstalkLogListener implements EventSubscriberInterface
 {
+    /**
+     * @var \Symfony\Bridge\Monolog\Logger
+     */
     protected $logger;
     
-    protected $listenEvents = array(
-        CommandEvent::BURY,
-        CommandEvent::DELETE,
-        CommandEvent::IGNORE,
-        CommandEvent::KICK,
-        CommandEvent::LIST_TUBES,
-        CommandEvent::LIST_TUBES_WATCHED,
-        CommandEvent::LIST_TUBE_USED,
-        CommandEvent::PEEK,
-        CommandEvent::PEEK_BURIED,
-        CommandEvent::PEEK_DELAYED,
-        CommandEvent::PEEK_READY,
-        CommandEvent::PUT,
-        CommandEvent::PUT_IN_TUBE,
-        CommandEvent::RELEASE,
-        CommandEvent::RESERVE,
-        CommandEvent::STATS,
-        CommandEvent::STATS_JOB,
-        CommandEvent::STATS_TUBE,
-        CommandEvent::TOUCH,
-        CommandEvent::USE_TUBE,
-        CommandEvent::WATCH,
-        CommandEvent::WATCH_ONLY,
-    );
-
-    public function getSubscribedEvents()
+    public static function getSubscribedEvents()
     {
-        return $this->listenEvents;
+        $listenEvents = array(     
+            CommandEvent::BURY => 'onCommand',
+            CommandEvent::DELETE => 'onCommand', 
+            CommandEvent::IGNORE => 'onCommand',
+            CommandEvent::KICK => 'onCommand',
+            CommandEvent::LIST_TUBE_USED => 'onCommand',
+            CommandEvent::LIST_TUBES => 'onCommand',
+            CommandEvent::LIST_TUBES_WATCHED => 'onCommand',
+            CommandEvent::PAUSE_TUBE => 'onCommand',
+            CommandEvent::PEEK => 'onCommand',
+            CommandEvent::PEEK_READY => 'onCommand',
+            CommandEvent::PEEK_DELAYED => 'onCommand',
+            CommandEvent::PEEK_BURIED => 'onCommand',
+            CommandEvent::PUT => 'onCommand',
+            CommandEvent::PUT_IN_TUBE => 'onCommand',
+            CommandEvent::RELEASE => 'onCommand',
+            CommandEvent::RESERVE => 'onCommand',
+            CommandEvent::RESERVE_FROM_TUBE => 'onCommand',
+            CommandEvent::STATS => 'onCommand',
+            CommandEvent::STATS_TUBE => 'onCommand',
+            CommandEvent::STATS_JOB => 'onCommand',
+            CommandEvent::TOUCH => 'onCommand',
+            CommandEvent::USE_TUBE => 'onCommand',
+            CommandEvent::WATCH => 'onCommand',
+            CommandEvent::WATCH_ONLY => 'onCommand',
+        );
+        
+        return $listenEvents;
     }
     
     /**
      * 
      * @param \Leezy\PheanstalkBundle\Event\CommandEvent $eventArgs
      */
-    protected function wildcardListener(CommandEvent $event)
+    public function onCommand(CommandEvent $event)
     {
+        if(!$this->getLogger()) {
+            return;
+        }
+        
         $pheanstalk = $event->getPheanstalk();
         $connection = $pheanstalk->getConnection();
         
         if(!$connection->isServiceListening()) {
-            $this->getLogger()->warn();
+            $this->getLogger()->warning('Pheanstalk connection isn\'t linstening');
+        }
+        
+        $pheanstalkName = 'unknown';        
+        if($pheanstalk instanceof PheanstalkProxyInterface) {
+            $pheanstalkName = $pheanstalk->getName();
         }
         
         $nameExploded = explode(".", $event->getName());
         
-        $this->getLogger()->log('Command: ' . $nameExploded[count($nameExploded) - 1]);
+        $this->getLogger()->info('Pheanstalk command: ' . $nameExploded[count($nameExploded) - 1], array (
+            'payload' => $event->getPayload(),
+            'pheanstalk' => $pheanstalkName,
+        ));
     }
     
-    public function __call($name, $arguments)
-    {
-        if(in_array($name, $this->listenEvents)) {
-            call_user_func_array(array($this, 'wildcardListener'), $arguments);
-        }
-    }
-    
+    /**
+     * @return \Symfony\Bridge\Monolog\Logger
+     */
     public function getLogger()
     {
-        
+        return $this->logger;
     }
     
+    /**
+     * 
+     * @param \Symfony\Bridge\Monolog\Logger $logger
+     */
     public function setLogger($logger)
     {
-        
+        $this->logger = $logger;
     }
 }
