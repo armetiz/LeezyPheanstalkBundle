1d66704c3cc1c39cb048ce3cf1e75917ab007f7f
Maxime Aoustin <max44410@gmail.com>
2013-04-03 19:01:10 +1100

    Update kick command to match coding standard

--- Command/KickCommand.php
+++ Command/KickCommand.php
@@ -18,7 +18,7 @@ class KickCommand extends ContainerAwareCommand
             ->setName('leezy:pheanstalk:kick')
             ->addArgument('tube', InputArgument::REQUIRED, 'The tube to kick the jobs from.')
             ->addArgument('max', InputArgument::OPTIONAL, 'The maximum job to kick from this tube.', 1)
-            ->addArgument('connection', InputArgument::OPTIONAL, 'Connection name.', "default")
+            ->addArgument('pheanstalk', InputArgument::OPTIONAL, 'Pheanstalk name.')
             ->setDescription('Kick buried jobs from a specific tube.')
         ;
     }
@@ -27,24 +27,40 @@ class KickCommand extends ContainerAwareCommand
     {
         $tube = $input->getArgument('tube');
         $max = $input->getArgument('max');
-        $connectionName = $input->getArgument('connection');
-        
-        $connectionLocator = $this->getContainer()->get('leezy.pheanstalk.connection_locator');
-        $pheanstalk = $connectionLocator->getConnection($connectionName);
-        
-        if (null == $pheanstalk) {
-            $output->writeln('Connection not found : <error>' . $connectionName . '</error>');
+        $pheanstalkName = $input->getArgument('pheanstalk');
+
+        $pheanstalkLocator = $this->getContainer()->get('leezy.pheanstalk.pheanstalk_locator');
+        $pheanstalk = $pheanstalkLocator->getConnection($pheanstalkName);
+
+        if (null === $pheanstalk) {
+            $output->writeln('Pheanstalk not found : <error>' . $pheanstalkName . '</error>');
             return;
         }
 
-        /** @var \Pheanstalk_Pheanstalk $pheanstalk */
-        $kicked = $pheanstalk->useTube($tube)->kick($max);
-
-        if ($kicked > 0) {
-            $output->writeln(sprintf('%d Job(s) have been kicked from %s', $kicked, $tube));
+        if (!$pheanstalk->getPheanstalk()->getConnection()->isServiceListening()) {
+            $output->writeln('Pheanstalk not connected : <error>' . $pheanstalkName . '</error>');
+            return;
         }
-        else {
-            $output->writeln('No job to kicked were found');
+
+        try
+        {
+            $numJobKicked = 0;
+            $numJobKicked = $pheanstalk->useTube($tube)->kick($max);
+
+            $output->writeln('Pheanstalk : <info>' . $pheanstalkName . '</info>');
+
+            if ($numJobKicked > 0) {
+                $output->writeln(sprintf('%d Job(s) have been kicked from %s', $numJobKicked, $tube));
+            }
+            else {
+                $output->writeln('No job to kicked were found');
+            }
+        }
+        catch(Pheanstalk_Exception_PheanstalkException $e)
+        {
+            $output->writeln('Pheanstalk : <info>' . $pheanstalkName . '</info>');
+            $output->writeln(sprintf('%d Job(s) have been kicked from %s', $numJobKicked, $tube));
+            $output->writeln(sprintf('<error>%s</error>', $e->getMessage()));
         }
     }
 }
