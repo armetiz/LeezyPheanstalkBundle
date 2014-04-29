c3cc20b045f1c43b39245e84d9c1594867eb166d
Robert-Jan Bijl <robert-jan@prezent.nl>
2014-04-29 08:39:48 +0200

    StatsTube shows stats for all tube if no argument is given
    If the 'tube' argument is omitted, the StatsTubeCommand will display statistics for all tubes in the given Pheanstalk

--- Command/StatsTubeCommand.php
+++ Command/StatsTubeCommand.php
@@ -18,22 +18,17 @@ class StatsTubeCommand extends ContainerAwareCommand
     {
         $this
             ->setName('leezy:pheanstalk:stats-tube')
-            ->addArgument('tube', InputArgument::REQUIRED, 'Tube to get stats.')
-            ->addArgument('pheanstalk', InputArgument::OPTIONAL, 'Pheanstalk name.')
+            ->addArgument('tube', InputArgument::OPTIONAL, 'Tube to get stats.', null)
+            ->addArgument('pheanstalk', InputArgument::OPTIONAL, 'Pheanstalk name.', 'default')
             ->setDescription('Gives statistical information about the specified tube if it exists.');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $tube = $input->getArgument('tube');
         $pheanstalkName = $input->getArgument('pheanstalk');
 
         $pheanstalkLocator = $this->getContainer()->get('leezy.pheanstalk.pheanstalk_locator');
         $pheanstalk = $pheanstalkLocator->getPheanstalk($pheanstalkName);
-
-        if (null === $pheanstalkName) {
-            $pheanstalkName = 'default';
-        }
 
         if (null === $pheanstalk) {
             $output->writeln('Pheanstalk not found : <error>' . $pheanstalkName . '</error>');
@@ -47,21 +42,40 @@ class StatsTubeCommand extends ContainerAwareCommand
             return;
         }
 
+        $output->writeln('Pheanstalk : <info>' . $pheanstalkName . '</info>');
+
         try {
-            $stats = $pheanstalk->statsTube($tube);
+            $tubes = array();
 
-            if (count($stats) === 0 ) {
-                $output->writeln('Pheanstalk : <error>' . $pheanstalkName . '</error>');
-                $output->writeln('<info>0 stats.</info>');
+            // if a tube argument is given, only consider that tube. If not, get stats for all tubes
+            if ($tube = $input->getArgument('tube')) {
+                $tubes[] = $tube;
             } else {
-                $output->writeln('Pheanstalk : <info>' . $pheanstalkName . '</info>');
+                $tubes = $pheanstalk->listTubes();
+            };
 
-                foreach ($stats as $key => $information) {
-                    $output->writeln('- <info>' . $key . '</info> : ' . $information);
+            $whiteline = false;
+            foreach ($tubes as $tube) {
+                // fetch stats for each tube
+                $stats = (array) $pheanstalk->statsTube($tube);
+
+                if (count($stats) === 0 ) {
+                    $output->writeln('Tube : <error>' . $tube . '</error>');
+                    $output->writeln('<info>0 stats.</info>');
+                } else {
+                    // only add a whiteline if we are past the first tube (for BC)
+                    if ($whiteline) {
+                        $output->writeln('');
+                    }
+
+                    foreach ($stats as $key => $information) {
+                        $output->writeln('- <info>' . $key . '</info> : ' . $information);
+                    }
                 }
+
+                $whiteline = true;
             }
         } catch (Pheanstalk_Exception $e) {
-            $output->writeln('Pheanstalk : <info>' . $pheanstalkName . '</info>');
             $output->writeln(sprintf('<error>%s</error>', $e->getMessage()));
         }
     }
